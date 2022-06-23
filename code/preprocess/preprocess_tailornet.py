65657ea8185d0d20900156a988c391b8b4f335ba
rui <jiangr101@gmail.com>
2022-06-23 13:02:12 +0300

    finished preprocess tailornet data

--- code/preprocess/preprocess_tailornet.py
+++ code/preprocess/preprocess_tailornet.py
@@ -7,9 +7,16 @@ from scipy.spatial import cKDTree
 import argparse
 
 import trimesh
+from scipy.spatial import cKDTree
+
+'''
+from CGAL.CGAL_Kernel import Point_3
+from CGAL.CGAL_Kernel import Triangle_3
+from CGAL.CGAL_AABB_tree import AABB_tree_Triangle_3_soup
+'''
+# need to run it under the cgal path
 
 import open3d as o3d
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -22,9 +29,9 @@ if __name__ == '__main__':
     for split in data_splits:
         print('split: ', split)
 
-        pkl_path = 'D:\\tailornet_dataset_pkl\\%s\\old-t-shirt_female' % split
-        idle_dir = 'D:\\tailornet_dataset_pkl\\idle\\old-t-shirt_female'
-        save_path = 'D:\\tailornet_dataset_pkl\\SAL_preprocessed\\%s' % split
+        pkl_path = '/media/Data/data/tailornet_dataset_pkl/%s/old-t-shirt_female' % split
+        idle_dir = '/media/Data/data/tailornet_dataset_pkl/idle/old-t-shirt_female'
+        save_path = '/media/Data/data/tailornet_dataset_pkl/SAL_preprocessed/%s' % split
 
         for pkl in os.listdir(pkl_path):
             if not pkl.endswith('.pkl'):
@@ -63,20 +70,45 @@ if __name__ == '__main__':
 
             o3d_pc = o3d.geometry.PointCloud()
             o3d_pc.points = o3d.utility.Vector3dVector(pnts)
-
             query_points = o3d.geometry.PointCloud()
             query_points.points = o3d.utility.Vector3dVector(sample)
-
             dists_o3d = np.asarray(query_points.compute_point_cloud_distance(o3d_pc))
 
+            '''
+            start_time = time.time()
+            _, dists_trimesh, _ = trimesh.proximity.closest_point(mesh, sample)
+            dists_trimesh = np.array(dists_trimesh)
+
+            print('trimesh time elasped ', time.time() - start_time)
             start_time = time.time()
 
-            _, dists, _ = trimesh.proximity.closest_point(mesh, sample)
+            triangles = []
+            for tri in mesh.triangles:
+                a = Point_3((tri[0][0] - center[0]) / scale, (tri[0][1] - center[1]) / scale,
+                            (tri[0][2] - center[2]) / scale)
+                b = Point_3((tri[1][0] - center[0]) / scale, (tri[1][1] - center[1]) / scale,
+                            (tri[1][2] - center[2]) / scale)
+                c = Point_3((tri[2][0] - center[0]) / scale, (tri[2][1] - center[1]) / scale,
+                            (tri[2][2] - center[2]) / scale)
+                triangles.append(Triangle_3(a, b, c))
+            tree = AABB_tree_Triangle_3_soup(triangles)
 
+            dists = []
+            for np_query in sample:
+                cgal_query = Point_3(np_query[0].astype(np.double), np_query[1].astype(np.double),
+                                     np_query[2].astype(np.double))
+
+                cp = tree.closest_point(cgal_query)
+                cp = np.array([cp.x(), cp.y(), cp.z()])
+                dist = np.sqrt(((cp - np_query) ** 2).sum(axis=0))
+
+                dists.append(dist)
             dists = np.array(dists)
 
-            print(max(dists_o3d - dists))
+            print(max(dists - dists_trimesh))
+            print('time elapsed CGAL ', time.time() - start_time)
             exit(0)
+            '''
 
             if not os.path.exists(save_path):
                 os.makedirs(save_path)
@@ -84,11 +116,11 @@ if __name__ == '__main__':
             file_name = pkl.split('.')[0]
             np.save(str(Path(save_path + '/' + file_name + '.npy', pnts)))
             np.save(str(Path(save_path + '/' + file_name + '_dist_triangle.npy')),
-                    np.concatenate([sample, np.expand_dims(dists, axis=-1)], axis=-1))
+                    np.concatenate([sample, np.expand_dims(dists_o3d, axis=-1)], axis=-1))
             np.save(str(Path(save_path + '/' + file_name + '_normalization.npy')),
                     {"center": center, "scale": scale})
 
-            print('done processing %s, time elapsed %f' % (pkl, time.time()-start_time))
+            print('done processing %s, time elapsed %f' % (pkl, time.time() - start_time))
 
 
 
